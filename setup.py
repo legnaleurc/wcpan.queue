9de45eea513fff61d76b99d83c9a3b510835b662
Wei-Cheng Pan <legnaleurc@gmail.com>
2016-10-05 23:10:06 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ from distutils.core import setup
 setup(name='tornado_multithread',
       version='1.0',
       packages=['tornado_multithread'],
-      )
+      install_requires=[
+	'tornado',
+      ])
