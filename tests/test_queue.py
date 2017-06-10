03014a15af78944c9b08da95357524bbe31ab114
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-06-11 03:18:59 +0900

    update

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,3 +1,5 @@
+import time
+
 from tornado import gen as tg, testing as tt
 
 import wcpan.worker as ww
@@ -8,20 +10,45 @@ class TestAsyncQueue(tt.AsyncTestCase):
 
     def setUp(self):
         super(TestAsyncQueue, self).setUp()
-        self._queue = ww.AsyncQueue()
-        self._queue.start()
 
     def tearDown(self):
         super(TestAsyncQueue, self).tearDown()
 
     @tt.gen_test
     def testPost(self):
+        self._queue = ww.AsyncQueue()
+        self._queue.start()
+
         fn = u.NonBlocker()
         rc = u.ResultCollector()
 
         self._queue.post(fn)
         self._queue.post(rc)
         yield rc.wait()
+        yield self._queue.stop()
+
         self.assertEqual(fn.call_count, 1)
 
+    @tt.gen_test
+    def testPost(self):
+        self._queue = ww.AsyncQueue(2)
+        self._queue.start()
+
+
+        rc = u.ResultCollector()
+        async def wait_one_second():
+            await tg.sleep(0.25)
+            rc.add(42)
+
+        self._queue.post(wait_one_second)
+        self._queue.post(wait_one_second)
+
+        before = time.perf_counter()
+        self._queue.post(rc)
+        yield rc.wait()
+        after = time.perf_counter()
+        diff = after - before
         yield self._queue.stop()
+
+        self.assertLess(diff, 0.3)
+        self.assertEqual(rc.values, [42, 42])
--- wcpan/worker/queue.py
+++ wcpan/worker/queue.py
@@ -1,3 +1,5 @@
+import functools as ft
+
 from tornado import queues as tq, locks as tl, ioloop as ti
 from wcpan.logger import EXCEPTION
 
@@ -17,7 +19,7 @@ class AsyncQueue(object):
     def start(self):
         if self._running:
             return
-        self._loop.add_callback(self._run)
+        self._loop.add_callback(self._process)
         self._running = True
 
     async def stop(self):
@@ -25,6 +27,7 @@ class AsyncQueue(object):
         self._running = False
         for i in range(self._max):
             self._queue.put_nowait(task)
+        self._end = tl.Event()
         await self._end.wait()
         self._reset()
 
@@ -32,21 +35,27 @@ class AsyncQueue(object):
         task = ensure_task(task)
         self._queue.put_nowait(task)
 
-    async def _run(self):
+    async def _process(self):
         while self._running:
-            async with self._lock:
-                task = await self._queue.get()
-                try:
-                    if isinstance(task, TerminalTask):
-                        break
-                    else:
-                        await regular_call(task)
-                except Exception as e:
-                    EXCEPTION('wcpan.worker', e) << 'uncaught exception'
-                finally:
-                    self._queue.task_done()
-        self._end.set()
+            await self._lock.acquire()
+            task = await self._queue.get()
+            fn = ft.partial(self._run, task)
+            self._loop.add_callback(fn)
+
+    async def _run(self, task):
+        try:
+            if isinstance(task, TerminalTask):
+                return
+            else:
+                await regular_call(task)
+        except Exception as e:
+            EXCEPTION('wcpan.worker', e) << 'uncaught exception'
+        finally:
+            self._queue.task_done()
+            self._lock.release()
+            if self._end and self._lock._value == self._max:
+                self._end.set()
 
     def _reset(self):
         self._queue = tq.PriorityQueue()
-        self._end = tl.Event()
+        self._end = None
