01fa5f0852604a99067bcaf75d63d814167767db
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-06-16 04:51:27 +0900

    fix

--- wcpan/worker/__init__.py
+++ wcpan/worker/__init__.py
@@ -1,4 +1,4 @@
 from .worker import AsyncWorker, WorkerError
-from .pool import AsyncWorkerPool
+from .pool import AsyncWorkerPool, create_thread_pool, off_main_thread_method
 from .queue import AsyncQueue
 from .task import Task
