58b7965dfe80f63ee1215e76a1d33393218da059
Wei-Cheng Pan <legnaleurc@gmail.com>
2018-07-05 07:04:17 +0900

    reimplement

--- wcpan/worker/queue.py
+++ wcpan/worker/queue.py
@@ -1,4 +1,5 @@
 import asyncio
+import contextlib as cl
 from typing import Callable
 
 from wcpan.logger import DEBUG, EXCEPTION
@@ -10,23 +11,34 @@ class AsyncQueue(object):
 
     def __init__(self, maximum=None):
         self._max = 1 if maximum is None else maximum
-        self._lock = asyncio.Semaphore(self._max)
-        self._loop = asyncio.get_event_loop()
         self._reset()
 
     def start(self):
-        if self._runner or self._stopper:
+        if self._consumer_list:
             return
-        self._runner = self._loop.create_task(self._process())
+
+        loop = asyncio.get_event_loop()
+        self._consumer_list = [
+            loop.create_task(self._consume())
+                for _ in range(self._max)
+        ]
 
     async def stop(self):
-        if not self._runner or self._stopper:
+        if not self._consumer_list or self._waiting_finish:
             return
-        task = TerminalTask()
-        self._queue.put_nowait(task)
-        self._stopper = asyncio.Event()
-        await self._stopper.wait()
-        self._runner.cancel()
+
+        # if all consumers are busy, we need to flush all pending tasks
+        self._set_internal_queue([])
+
+        # if some consumers are busy, we need to wait for running tasks
+        if not self.idle:
+            self._waiting_finish = asyncio.Event()
+            await self._waiting_finish.wait()
+
+        # cancel all consumers
+        for consumer in self._consumer_list:
+            consumer.cancel()
+
         self._reset()
 
     def flush(self, filter_: Callable[['Task'], bool]):
@@ -37,36 +49,54 @@ class AsyncQueue(object):
         self._set_internal_queue(nq)
 
     def post(self, task: MaybeTask):
+        if self._waiting_finish:
+            return
+
+        self.start()
+
         task = ensure_task(task)
         self._queue.put_nowait(task)
 
-    async def _process(self):
-        while self._runner:
-            await self._lock.acquire()
-            task = await self._queue.get()
-            self._loop.create_task(self._run(task))
+    @property
+    def idle(self):
+        return self._active_count == 0
 
-    async def _run(self, task):
-        try:
-            if isinstance(task, TerminalTask):
-                return
-            else:
-                await regular_call(task)
-        except Exception as e:
-            EXCEPTION('wcpan.worker', e) << 'uncaught exception'
-        finally:
-            self._queue.task_done()
-            self._lock.release()
-            if self._stopper:
-                self._stopper.set()
+    async def _consume(self):
+        while True:
+            async with self._pop_task() as task, \
+                       self._active_guard():
+                try:
+                    await regular_call(task)
+                except Exception as e:
+                    EXCEPTION('wcpan.worker', e) << 'uncaught exception'
+
+            if self._waiting_finish and self.idle:
+                self._waiting_finish.set()
 
     def _reset(self):
         self._queue = asyncio.PriorityQueue()
-        self._stopper = None
-        self._runner = None
+        self._active_count = 0
+        self._waiting_finish = None
+        self._consumer_list = None
 
     def _get_internal_queue(self):
         return self._queue._queue
 
     def _set_internal_queue(self, nq):
         self._queue._queue = nq
+
+    @cl.asynccontextmanager
+    async def _active_guard(self):
+        self._active_count += 1
+        try:
+            yield
+        finally:
+            self._active_count -= 1
+
+    @cl.asynccontextmanager
+    async def _pop_task(self):
+        task = await self._queue.get()
+        try:
+            yield task
+        finally:
+            self._queue.task_done()
