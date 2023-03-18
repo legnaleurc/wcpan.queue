00de05bd9a75b2810930542a90144adc72ecdf8d
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-03-19 00:11:43 +0900

    add new method

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -22,12 +22,20 @@ def create_tree() -> Node:
     return r
 
 
-async def walk_tree(rv: list[int], q: AioQueue, n: Node | None):
+async def void_walk_tree(rv: list[int], q: AioQueue, n: Node | None) -> None:
     if not n:
         return
     rv.append(n.v)
-    await q.push(walk_tree(rv, q, n.l))
-    await q.push(walk_tree(rv, q, n.r))
+    await q.push(void_walk_tree(rv, q, n.l))
+    await q.push(void_walk_tree(rv, q, n.r))
+
+
+async def walk_tree(q: AioQueue[int], n: Node) -> int:
+    if n.l:
+        await q.push(walk_tree(q, n.l))
+    if n.r:
+        await q.push(walk_tree(q, n.r))
+    return n.v
 
 
 async def long_task(rv: list[int]):
@@ -40,8 +48,12 @@ async def long_task(rv: list[int]):
         raise
 
 
-async def plain_task(rv: list[str], value: str):
+async def void_task(rv: list[str], value: str):
     rv.append(value)
+
+
+async def return_task(value: str):
+    return value
 
 
 async def bad_task():
@@ -57,6 +69,9 @@ class AioQueueTestCase(IsolatedAsyncioTestCase):
         q = AioQueue.fifo()
         with self.assertRaises(ValueError):
             await q.consume()
+        with self.assertRaises(ValueError):
+            async for _ in q.collect():
+                pass
 
     async def test_bad_consumer_size(self):
         q = AioQueue.fifo()
@@ -65,26 +80,67 @@ class AioQueueTestCase(IsolatedAsyncioTestCase):
             await q.consume(-1)
         q.purge()
 
-    async def test_exception(self):
+    async def test_bad_collector_size(self):
+        q = AioQueue.fifo()
+        await q.push(nop())
+        with self.assertRaises(ValueError):
+            async for _ in q.collect(-1):
+                pass
+        q.purge()
+
+    async def test_consume_exception(self):
         q = AioQueue.fifo()
         await q.push(bad_task())
         with self.assertRaises(ExceptionGroup) as e:
             await q.consume()
         self.assertEqual(q.size, 0)
 
-    async def test_recursive(self):
+    async def test_collect_exception(self):
+        q = AioQueue.fifo()
+        await q.push(bad_task())
+        with self.assertRaises(ExceptionGroup):
+            async for _ in q.collect():
+                pass
+
+    async def test_consume_recursive(self):
         tree = create_tree()
         q = AioQueue.fifo()
         rv: list[int] = []
-        await q.push(walk_tree(rv, q, tree))
+        await q.push(void_walk_tree(rv, q, tree))
         await q.consume()
         self.assertEqual(rv, [0, 1, 2, 3, 4, 5, 6])
 
-    async def test_cancel(self):
+    async def test_collect_recursive(self):
+        tree = create_tree()
+        q = AioQueue[int].fifo()
+        await q.push(walk_tree(q, tree))
+        rv = [_ async for _ in q.collect()]
+        self.assertEqual(rv, [0, 1, 2, 3, 4, 5, 6])
+
+    async def test_consume_cancel(self):
         q = AioQueue.fifo()
         rv: list[int] = []
         await q.push(long_task(rv))
         task = create_task(q.consume())
+        await sleep(0)
+        task.cancel()
+        try:
+            await task
+        except CancelledError:
+            pass
+        self.assertEqual(rv, [0, 2])
+        self.assertEqual(q.size, 0)
+
+    async def test_collect_cancel(self):
+        q = AioQueue.fifo()
+        rv: list[int] = []
+        await q.push(long_task(rv))
+
+        async def to_coro():
+            async for _ in q.collect():
+                pass
+
+        task = create_task(to_coro())
         await sleep(0)
         task.cancel()
         try:
@@ -100,22 +156,40 @@ class AioQueueTestCase(IsolatedAsyncioTestCase):
         q.purge()
         self.assertEqual(q.size, 0)
 
-    async def test_priority(self):
+    async def test_consume_priority(self):
         q = AioQueue.priority()
         rv: list[str] = []
-        await q.push(plain_task(rv, "a"), 0)
-        await q.push(plain_task(rv, "b"), 2)
-        await q.push(plain_task(rv, "c"), 1)
-        await q.push(plain_task(rv, "d"), 0)
+        await q.push(void_task(rv, "a"), 0)
+        await q.push(void_task(rv, "b"), 2)
+        await q.push(void_task(rv, "c"), 1)
+        await q.push(void_task(rv, "d"), 0)
         await q.consume()
         self.assertEqual(rv, ["a", "d", "c", "b"])
 
-    async def test_lifo(self):
+    async def test_collect_priority(self):
+        q = AioQueue[str].priority()
+        await q.push(return_task("a"), 0)
+        await q.push(return_task("b"), 2)
+        await q.push(return_task("c"), 1)
+        await q.push(return_task("d"), 0)
+        rv = [_ async for _ in q.collect()]
+        self.assertEqual(rv, ["a", "d", "c", "b"])
+
+    async def test_consume_lifo(self):
         q = AioQueue.lifo()
         rv: list[str] = []
-        await q.push(plain_task(rv, "a"))
-        await q.push(plain_task(rv, "b"))
-        await q.push(plain_task(rv, "c"))
-        await q.push(plain_task(rv, "d"))
+        await q.push(void_task(rv, "a"))
+        await q.push(void_task(rv, "b"))
+        await q.push(void_task(rv, "c"))
+        await q.push(void_task(rv, "d"))
         await q.consume()
+        self.assertEqual(rv, ["d", "c", "b", "a"])
+
+    async def test_collect_lifo(self):
+        q = AioQueue[str].lifo()
+        await q.push(return_task("a"))
+        await q.push(return_task("b"))
+        await q.push(return_task("c"))
+        await q.push(return_task("d"))
+        rv = [_ async for _ in q.collect()]
         self.assertEqual(rv, ["d", "c", "b", "a"])
--- wcpan/queue/__init__.py
+++ wcpan/queue/__init__.py
@@ -1,31 +1,34 @@
 from asyncio import LifoQueue, PriorityQueue, Queue, Task, TaskGroup
+from collections.abc import AsyncGenerator, Coroutine
+from contextlib import asynccontextmanager
 from importlib.metadata import version
-from typing import Coroutine, Self, TypeAlias
+from typing import Generic, Self, TypeAlias, TypeVar
 
 
 __version__ = version(__package__ or __name__)
 __all__ = ("AioQueue",)
 
 
-Runnable: TypeAlias = Coroutine[None, None, None]
-_SortableRunnable: TypeAlias = tuple[int, int, Runnable]
-_Queue: TypeAlias = Queue[_SortableRunnable]
+_T = TypeVar("_T")
+AioCoroutine: TypeAlias = Coroutine[None, None, _T]
+_SortableJob: TypeAlias = tuple[int, int, AioCoroutine[_T]]
+_Queue: TypeAlias = Queue[_SortableJob[_T]]
 
 
-class AioQueue:
+class AioQueue(Generic[_T]):
     @classmethod
     def fifo(cls, maxsize: int = 0) -> Self:
-        return AioQueue(Queue(maxsize))
+        return AioQueue[_T](Queue(maxsize))
 
     @classmethod
     def priority(cls, maxsize: int = 0) -> Self:
-        return AioQueue(PriorityQueue(maxsize))
+        return AioQueue[_T](PriorityQueue(maxsize))
 
     @classmethod
     def lifo(cls, maxsize: int = 0) -> Self:
-        return AioQueue(LifoQueue(maxsize))
+        return AioQueue[_T](LifoQueue(maxsize))
 
-    def __init__(self, queue: _Queue) -> None:
+    def __init__(self, queue: _Queue[_T]) -> None:
         self._id = 0
         self._queue = queue
 
@@ -51,32 +54,42 @@ class AioQueue:
     def __exit__(self, e, et, tb) -> None:
         self.purge()
 
-    async def push(self, runnable: Runnable, priority: int = 0) -> None:
+    async def push(self, coro: AioCoroutine[_T], priority: int = 0) -> None:
         self._id = self._id + 1
-        await self._queue.put((priority, self._id, runnable))
+        await self._queue.put((priority, self._id, coro))
 
-    def push_nowait(self, runnable: Runnable, priority: int = 0) -> None:
+    def push_nowait(self, coro: AioCoroutine[_T], priority: int = 0) -> None:
         self._id = self._id + 1
-        self._queue.put_nowait((priority, self._id, runnable))
+        self._queue.put_nowait((priority, self._id, coro))
 
     async def consume(self, maxsize: int = 1) -> None:
-        if self._queue.empty():
+        if self.empty:
             raise ValueError(f"queue must have something to consume")
 
         if maxsize <= 0:
             raise ValueError(f"invalid consumer size: {maxsize}")
 
-        async with TaskGroup() as tg:
-            consumer_list: list[Task[None]] = []
-            while maxsize > 0:
-                task = tg.create_task(_consume(self._queue))
-                consumer_list.append(task)
-                maxsize -= 1
-            try:
-                await self._queue.join()
-            finally:
-                for task in consumer_list:
-                    task.cancel()
+        async with _spawn(maxsize, _consume(self._queue)):
+            await self._queue.join()
+
+    async def collect(self, maxsize: int = 1) -> AsyncGenerator[_T, None]:
+        if self.empty:
+            raise ValueError(f"queue must have something to collect")
+
+        if maxsize <= 0:
+            raise ValueError(f"invalid collector size: {maxsize}")
+
+        result = Queue[_T]()
+        async with _spawn(maxsize, _collect(self._queue, result)) as group:
+            producer = group.create_task(self._queue.join())
+            consumer = group.create_task(result.join())
+            while not producer.done() or not result.empty():
+                rv = await result.get()
+                try:
+                    yield rv
+                finally:
+                    result.task_done()
+            await consumer
 
     def purge(self) -> None:
         while not self._queue.empty():
@@ -85,10 +98,35 @@ class AioQueue:
             self._queue.task_done()
 
 
-async def _consume(q: _Queue) -> None:
+@asynccontextmanager
+async def _spawn(maxsize: int, worker: AioCoroutine):
+    async with TaskGroup() as group:
+        task_list: list[Task[None]] = []
+        while maxsize > 0:
+            task = group.create_task(worker)
+            task_list.append(task)
+            maxsize -= 1
+        try:
+            yield group
+        finally:
+            for task in task_list:
+                task.cancel()
+
+
+async def _consume(q: _Queue[_T]) -> None:
     while True:
         (_p, _i, cb) = await q.get()
         try:
             await cb
         finally:
             q.task_done()
+
+
+async def _collect(iq: _Queue[_T], oq: Queue[_T]) -> None:
+    while True:
+        (_p, _i, cb) = await iq.get()
+        try:
+            rv = await cb
+            await oq.put(rv)
+        finally:
+            iq.task_done()
